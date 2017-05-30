c85a4ae4ee94963797247d95d40d76af2b61f103
Oliver Etchebarne <oliver@icaserver.com>
2017-05-30 05:06:25 -0500

    Prolog tag now use a ProcessingInstruction node

--- Document.php
+++ Document.php
@@ -2,28 +2,30 @@
 namespace drmad\semeele;
 
 /**
- * XML Root document node representation
+ * XML root node representation which include a prolog tag
  */
 class Document extends Node
 {
-    private $version;
-    private $encoding;
+    /** XML declaration node, as a processing instruction */
+    private $declaration;
 
     public function __construct($rootNodeName, $version = '1.0', $encoding = 'utf-8')
     {
-        $this->version = $version;
-        $this->encoding = $encoding;
+        $this->declaration = new ProcessingInstruction('xml', null, compact('version', 'encoding'));
+
         parent::__construct($rootNodeName);
+    }
+
+    /**
+     * Returns the XML declaration node, so you can add attributes
+     */
+    public function getDeclaration()
+    {
+        return $this->declaration;
     }
 
     public function getXML()
     {
-        // First draw the XML declaration
-        $xml = '<?xml version="' . $this->version . '" encoding="' . $this->encoding . '"?>' ;
-
-        // FIX: Is obligatory the \n?
-        $xml .= "\n";
-
-        return $xml . parent::getXML();
+        return $this->declaration->getXML() . parent::getXML();
     }
 }
